25db6e27f1d8438b6d7a738c752ff8ddfeb220e4
mhmad-jamal <mohammad52008@hotmail.com>
2023-07-04 16:40:58 +0300

    update subsecription

--- resources/views/subsicription/edit.blade.php
+++ resources/views/subsicription/edit.blade.php
@@ -78,7 +78,22 @@
             
             </div>
         </div>
+      
         <div class="card card-body border-0 shadow mb-4">
+            <div class="btn-toolbar mb-2 mb-md-0 d-flex justify-content-end">
+                <button href="#" class="btn btn-sm btn-gray-800 d-inline-flex align-items-center" data-toggle="modal"
+                    data-target="#exampleModal">
+                    اضافة اشتراك جديد
+                    <svg class="icon icon-xs me-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M12 6v6m0 0v6m0-6h6m-6 0H6">
+                        </path>
+                    </svg>
+                </button>
+    
+    
+            </div>
             <h2 class="h5 mb-4 text-center mb-5">جميع الأشتراكات</h2>
 
             @foreach ($subscriptions as $subscription )
@@ -138,7 +153,79 @@
 
         </div>
        </div>
-         
+       <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+       style="display: none;" aria-hidden="true">
+       <div class="modal-dialog" role="document">
+           <div class="modal-content">
+               <div class="modal-header">
+                   <button type="button" style="background: none;border: none;" class="close  "
+                       data-dismiss="modal" aria-label="Close">
+                       <span aria-hidden="true">×</span>
+                   </button>
+                   <h5 class="modal-title" id="exampleModalLabel">اضافة اشتراك جديد</h5>
+
+               </div>
+               <div class="modal-body" dir="rtl">
+                   <form action="{{ route('create_subsicription') }}" method="POST">
+                       @csrf
+                       <div class="form-group mb-4"">
+                           <label for="client_name">اسم العميل </label>
+                           <select disabled class="form-select mb-0" id="client_name" >
+                               <option disabled="" selected="" value="">اختار اسم العميل</option>
+                                   <option  selected value="{{ $user->id }}">{{ $user->name }}</option>
+
+                           </select>
+<input type="hidden" name="client_id" value="{{$user->id}}">
+                       </div>
+
+
+                       <!-- End of Form -->
+                       <!-- Form -->
+                       <div class="form-group mb-4">
+                           <label for="start_date">تاريخ البداية </label>
+                           <div class="input-group">
+                               <input type="date" class="form-control" id="start_date" name="start_date"
+                                   value="" required="">
+                           </div>
+                       </div>
+
+                       <div class="form-group mb-4">
+                           <label for="end_date">تاريخ النهاية</label>
+                           <div class="input-group">
+                               <input type="date" class="form-control" id="end_date" name="end_date"
+                                   value="" required="">
+                           </div>
+                       </div>
+                       <div class="form-group mb-4">
+                           <label for="end_date">القيمة </label>
+                           <div class="input-group">
+                               <input type="text" class="form-control" id="payment_amount"
+                                   name="payment_amount" value="" required="">
+                           </div>
+                       </div>
+
+
+
+
+
+
+               </div>
+
+               <div class="modal-footer">
+                   <button type="button" class="btn btn-secondary" data-dismiss="modal">اغلاق </button>
+                   <button type="submit" class="btn btn-primary">حفــظ</button>
+               </div>
+
+               </form>
+           </div>
+       </div>
+   </div>
+   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+            integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+        </script>
         {{-- Footer --}}
         @include('layouts.footer')
     </main>
